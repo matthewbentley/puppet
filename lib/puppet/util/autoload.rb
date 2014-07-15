ae617539a328749e5b9302860807618cec052d8a
Maksym Melnychok <maksym.melnychok@gmail.com>
2014-07-15 16:56:37 +0200

    cache search directories after initialization

--- lib/puppet/util/autoload.rb
+++ lib/puppet/util/autoload.rb
@@ -156,7 +156,17 @@ class Puppet::Util::Autoload
       gem_source.directories
     end
 
-    def search_directories(env)
+    def search_directories(env=nil)
+      @search_directories ||= {}
+      # cache only after initialization
+      if Puppet.settings.app_defaults_initialized?
+        @search_directories[env] ||= search_directories_uncached(env)
+      else
+        search_directories_uncached(env)
+      end
+    end
+
+    def search_directories_uncached(env)
       [gem_directories, module_directories(env), libdirs(), $LOAD_PATH].flatten
     end
 
